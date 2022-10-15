63c26854c237894c5037a29153f7c10e7679fad0
Seu Siqueira <rodrigolycan93@gmail.com>
2022-10-15 19:31:47 -0300

    adicionando tratamento de dados recebidos pelo form

--- script.php
+++ script.php
@@ -9,17 +9,38 @@ $categorias[] = 'idoso';
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
-var_dump($nome, $idade);
+if(empty($nome)){
+    echo 'o nome não pode estar em branco';
+    return;
+}
 
-// if($idade >= 6 && $idade <= 12)
-// {
-// 	echo "infantil";
-// }
-// else if($idade >= 13 && $idade <= 18) 
-// {
-// 	echo "adolescente";
-// }
-// else
-// {
-// 	echo "adulto";
-// }
+if(strlen($nome)<3){
+    echo 'é necessário ter mais que 3 caracteres';
+    return;
+}
+if(strlen($nome)>15){
+    echo 'você precisa digitar no máximo 15 caracteres';
+    return;
+}
+if(!is_numeric($idade)){
+    echo 'precisa ser em números';
+}
+if(strlen($idade)<100){
+    echo 'porra tu é velho em?!?';
+    return;
+}
+
+// var_dump($nome, $idade);
+
+if($idade >= 6 && $idade <= 12)
+{
+	echo "infantil";
+}
+else if($idade >= 13 && $idade <= 18) 
+{
+	echo "adolescente";
+}
+else
+{
+	echo "adulto";
+}
